39fb61bd6893a5657b776e301673a9013be86c8b
Yuki Kitamura <Pro.ktmr@gmail.com>
2026-07-15 11:07:13 +0900

    Support GA

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -4,7 +4,24 @@ import "@fontsource/roboto/400.css";
 import "@fontsource/roboto/500.css";
 import "@fontsource/roboto/700.css";
 import type { AppProps } from "next/app";
+import Script from "next/script";
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  return (
+    <>
+      <Script
+        src="https://www.googletagmanager.com/gtag/js?id=G-0YCEGH6ERT"
+        strategy="afterInteractive"
+      />
+      <Script id="google-analytics" strategy="afterInteractive">
+        {`
+          window.dataLayer = window.dataLayer || [];
+          function gtag(){dataLayer.push(arguments);}
+          gtag('js', new Date());
+          gtag('config', 'G-0YCEGH6ERT');
+        `}
+      </Script>
+      <Component {...pageProps} />
+    </>
+  );
 }
